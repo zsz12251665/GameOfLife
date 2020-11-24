e6287a15b4e24e3ca64ba99cc463d49e3da49f71
zsz12251665 <zsz12251665@hotmail.com>
2020-11-24 18:08:37 +0800

    Update GameOfLife.py

--- GameOfLife.py
+++ GameOfLife.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
-class GameOfLife:
+class PrototypeGame:
     def __init__(self, playground, *, neighborhoodRange=1, aliveSwitches=[-1, -1, 0, 1, -1, -1, -1, -1, -1], loopingBound=False):
         self.playground = (np.array(playground) > 127) * 255
         self.neighborhoodRange = neighborhoodRange
@@ -26,8 +26,7 @@ class GameOfLife:
     def nextFrame(self):
         n, m = self.playground.shape
         status = np.array([[self.aliveSwitches[self.countNeighourCells(x, y)] for y in range(m)] for x in range(n)])
-        self.playground = (status == 0) * self.playground + \
-            (status == 1) * 255 + (status == -1) * 0
+        self.playground = (status == 0) * self.playground + (status == 1) * 255 + (status == -1) * 0
 
     def startAnimation(self, *, interval=200):
         def update(*args):
@@ -38,7 +37,7 @@ class GameOfLife:
         fig = plt.figure()
         fig.canvas.set_window_title("Conway's Game of Life")
         im = plt.imshow(self.playground, animated=True)
-        animation.FuncAnimation(fig, update, interval=interval)
+        animation.FuncAnimation(fig, update, interval=interval, blit=True)
         plt.show()
 
     @staticmethod
@@ -61,9 +60,9 @@ class GameOfLife:
         return np.pad(arr, ((1, 1), (1, 1)))
 
 
-class ConwayGame(GameOfLife):
+class ConwayGame(PrototypeGame):
     def __init__(self, playground, *, loopingBound=False):
-        GameOfLife.__init__(self, playground, loopingBound=loopingBound)
+        PrototypeGame.__init__(self, playground, loopingBound=loopingBound)
 
     class commonModels:
         """
@@ -71,21 +70,21 @@ class ConwayGame(GameOfLife):
         See More: https://conwaylife.com/wiki/Category:Lists_of_patterns
         """
         class StillLifes:
-            Block = GameOfLife.buildPlayground("AA,AA")
-            Beehive = GameOfLife.buildPlayground("DAA,ADDA,ADDA,DAA")
-            Loaf = GameOfLife.buildPlayground("DAA,ADDA,DADA,DDA")
-            Boat = GameOfLife.buildPlayground("AA,ADA,DA")
-            Ship = GameOfLife.buildPlayground("AA,ADA,DAA")
-            Tub = GameOfLife.buildPlayground("DA,ADA,DA")
-            Pond = GameOfLife.buildPlayground("DAA,ADDA,DAA")
+            Block = PrototypeGame.buildPlayground("AA,AA")
+            Beehive = PrototypeGame.buildPlayground("DAA,ADDA,ADDA,DAA")
+            Loaf = PrototypeGame.buildPlayground("DAA,ADDA,DADA,DDA")
+            Boat = PrototypeGame.buildPlayground("AA,ADA,DA")
+            Ship = PrototypeGame.buildPlayground("AA,ADA,DAA")
+            Tub = PrototypeGame.buildPlayground("DA,ADA,DA")
+            Pond = PrototypeGame.buildPlayground("DAA,ADDA,DAA")
 
         class Oscillators:
-            Blinker = GameOfLife.buildPlayground(",3A,")
-            Toad = GameOfLife.buildPlayground(",D3A,3A,")
-            Beacon = GameOfLife.buildPlayground("AA,A,3DA,DDAA")
-            Pulsar = GameOfLife.buildPlayground(",3D3A3D3A,,DA4DADA4DA,DA4DADA4DA,DA4DADA4DA,3D3A3D3A,,3D3A3D3A,DA4DADA4DA,DA4DADA4DA,DA4DADA4DA,,3D3A3D3A,")
-            Pentadecathlon = GameOfLife.buildPlayground(",,,3D3A,DDA3DA,DA5DA,,A7DA,A7DA,,DA5DA,DDA3DA,3D3A,,,")
-            Clock = GameOfLife.buildPlayground("DDA,ADA,DADA,DA")
+            Blinker = PrototypeGame.buildPlayground(",3A,")
+            Toad = PrototypeGame.buildPlayground(",D3A,3A,")
+            Beacon = PrototypeGame.buildPlayground("AA,A,3DA,DDAA")
+            Pulsar = PrototypeGame.buildPlayground(",3D3A3D3A,,DA4DADA4DA,DA4DADA4DA,DA4DADA4DA,3D3A3D3A,,3D3A3D3A,DA4DADA4DA,DA4DADA4DA,DA4DADA4DA,,3D3A3D3A,")
+            Pentadecathlon = PrototypeGame.buildPlayground(",,,3D3A,DDA3DA,DA5DA,,A7DA,A7DA,,DA5DA,DDA3DA,3D3A,,,")
+            Clock = PrototypeGame.buildPlayground("DDA,ADA,DADA,DA")
 
         class Spaceships:
             """
@@ -94,13 +93,13 @@ class ConwayGame(GameOfLife):
             MWSS: Middle-weight Spaceship
             HWSS: Heavy-weight Spaceship
             """
-            Glider = GameOfLife.buildPlayground("DA,DDA,3A")
-            LWSS = GameOfLife.buildPlayground("ADDA,4DA,A3DA,D4A,")
-            MWSS = GameOfLife.buildPlayground("DA,A3DA,5DA,A4DA,D5A,,")
-            HWSS = GameOfLife.buildPlayground("DDAA,A4DA,6DA,A5DA,D6A,,")
+            Glider = PrototypeGame.buildPlayground("DA,DDA,3A")
+            LWSS = PrototypeGame.buildPlayground("ADDA,4DA,A3DA,D4A,")
+            MWSS = PrototypeGame.buildPlayground("DA,A3DA,5DA,A4DA,D5A,,")
+            HWSS = PrototypeGame.buildPlayground("DDAA,A4DA,6DA,A5DA,D6A,,")
 
         class Guns:
             """
             GGG: Gosper Glider Gun
             """
-            GGG = GameOfLife.buildPlayground("24DA,22DADA,12DAA6DAA12DAA,11DA3DA4DAA12DAA,AA8DA5DA3DAA,AA8DA3DADAA4DADA,10DA5DA7DA,11DA3DA,12DAA")
+            GGG = PrototypeGame.buildPlayground("24DA,22DADA,12DAA6DAA12DAA,11DA3DA4DAA12DAA,AA8DA5DA3DAA,AA8DA3DADAA4DADA,10DA5DA7DA,11DA3DA,12DAA")
