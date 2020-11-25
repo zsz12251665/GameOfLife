2e9371e9c1efd81f1787744715d3b93ca9c7a7bc
zsz12251665 <zsz12251665@hotmail.com>
2020-11-25 21:27:17 +0800

    Use tkinker to choose image

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from GameOfLife import ConwayGame, PrototypeGame
 from PIL import Image
-import matplotlib.pyplot as plt
+from tkinter.filedialog import askopenfilename
 
 conway = ConwayGame(ConwayGame.commonModels.Oscillators.Pentadecathlon, loopingBound=False)
 conway.startAnimation(interval=100)
@@ -13,6 +13,6 @@ conway.loopingBound = True # Change parameters
 conway.playground = np.tile(ConwayGame.buildPlayground("bo$2bo$3o!", charset="ob$"), (10, 10)) # RLE from conwaylife.com
 conway.startAnimation(interval=100)
 
-img = Image.open(input()).convert("L") # Choose your image as the playground
+img = Image.open(askopenfilename(title="Choose a file...")).convert("L") # Choose your image as the playground
 proto = PrototypeGame(img, neighborhoodRange=2, aliveSwitches=[-1] * 5 + [0, 0, 1, 1, 1] + [-1] * 14 + [0])  # Design your own game
 proto.startAnimation(showAnim=False, frames=50, saveAs={"filename": "test.gif", "writer": "imagemagick"})
